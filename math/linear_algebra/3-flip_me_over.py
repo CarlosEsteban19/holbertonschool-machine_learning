dae52e6bd06a202d3dbc92eff52922e16231844d
CarlosEsteban19 <carlos.esteban19@outlook.com>
2024-12-06 09:55:32 -0400

    transposing matrices

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -5,9 +5,12 @@
 def matrix_transpose(matrix):
     """returns the transpose of a 2D matrix"""
     transposed_matrix = []
+
     for i in range(len(matrix[0])):
         transposed_row = []
+
         for row in matrix:
             transposed_row.append(row[i])
+
         transposed_matrix.append(transposed_row)
     return transposed_matrix
