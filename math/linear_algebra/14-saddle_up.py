ed3dc9f8b7a26657eb08986e942f2e633e6e86ad
CarlosEsteban19 <carlos.esteban19@outlook.com>
2024-12-06 16:01:55 -0400

    matmul

--- math/linear_algebra/14-saddle_up.py
+++ math/linear_algebra/14-saddle_up.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """linear algebra task 14"""
+import numpy as np
 
 
 def np_matmul(mat1, mat2):
     """matrix multiplication"""
-    return mat1 * mat2
+    return np.matmul(mat1, mat2)
