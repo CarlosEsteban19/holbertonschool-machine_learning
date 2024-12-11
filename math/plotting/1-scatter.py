1964e10fb0fdf47cda7099b166ba462a13d52da3
CarlosEsteban19 <carlos.esteban19@outlook.com>
2024-12-11 13:13:47 -0400

    .

--- math/plotting/1-scatter.py
+++ math/plotting/1-scatter.py
@@ -13,11 +13,8 @@ def scatter():
     y += 180
     plt.figure(figsize=(6.4, 4.8))
 
-    plt.scatter(x, y, c='m')
+    plt.scatter(x, y, c='m', marker='.')
     plt.title("Men's Height vs Weight")
     plt.xlabel("Height (in)")
     plt.ylabel("Weight (lbs)")
     plt.show()
-
-
-scatter()
