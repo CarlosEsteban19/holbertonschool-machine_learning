3f3b0b3902cbbd6358e85068e2ea23b6dc601df5
CarlosEsteban19 <carlos.esteban19@outlook.com>
2024-12-11 11:47:51 -0400

    chmod

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -8,7 +8,7 @@ def line():
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
 
-    plt.plot(y, color='red', linestyle='-', linewidth=2)
+    plt.plot(y, color='red')
     plt.xlim(0, 10)
     plt.show()
 
