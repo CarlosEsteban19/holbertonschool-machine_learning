47a4d5b9cc96c70730272c55d18bdfd7904f34f5
CarlosEsteban19 <carlos.esteban19@outlook.com>
2024-12-15 17:03:12 -0400

    not recursion

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -4,12 +4,16 @@
 
 def summation_i_squared(n):
     """sumatoria al cuadrado"""
+    # border patrol
     if not isinstance(n, int) or n < 1:
         return None
 
     # Base case / turn around you recursive soab
-    if n == 1:
-        return 1
+    # if n == 1:
+    #     return 1
 
     # Recursioooooon (not a loop :D)
-    return n**2 + summation_i_squared(n - 1)
+    # return n**2 + summation_i_squared(n - 1)
+
+    # Using formula for the sum of squares because of RecursionError -___-
+    return (n * (n + 1) * (2 * n + 1)) // 6
