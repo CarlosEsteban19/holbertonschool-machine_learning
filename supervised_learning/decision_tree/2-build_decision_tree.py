349a41de8ebf993aa5ff9ed518c83d3db5a403b6
CarlosEsteban19 <carlos.esteban19@outlook.com>
2024-12-22 19:18:14 -0400

    extra space

--- supervised_learning/decision_tree/2-build_decision_tree.py
+++ supervised_learning/decision_tree/2-build_decision_tree.py
@@ -41,9 +41,9 @@ class Node:
     def left_child_add_prefix(self, text):
         """Add prefix for left child"""
         lines = text.split("\n")
-        new_text = "    +--"+lines[0]+"\n"
+        new_text = "    +--" + lines[0] + "\n"
         for x in lines[1:]:
-            new_text += ("    |  "+x)+"\n"
+            new_text += "    |  " + x + "\n"
         return (new_text)
 
     def right_child_add_prefix(self, text):
@@ -51,7 +51,7 @@ class Node:
         lines = text.split("\n")
         new_text = "    +--" + lines[0] + "\n"
         for x in lines[1:]:
-            new_text += ("       " + x) + "\n"
+            new_text += "       " + x + "\n"
         return new_text
 
     def __str__(self):
