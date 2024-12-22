096390cc50039a73e41592c7af01a95264a6b6ab
CarlosEsteban19 <carlos.esteban19@outlook.com>
2024-12-22 18:59:49 -0400

    tanteando

--- supervised_learning/decision_tree/2-build_decision_tree.py
+++ supervised_learning/decision_tree/2-build_decision_tree.py
@@ -49,7 +49,7 @@ class Node:
     def right_child_add_prefix(self, text):
         """Add prefix for right child"""
         lines = text.split("\n")
-        new_text = "    +---> " + lines[0] + "\n"
+        new_text = "   +---> " + lines[0] + "\n"
         for x in lines[1:]:
             new_text += "         " + x + "\n"
         return new_text
