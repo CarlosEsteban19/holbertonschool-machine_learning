9eddbd16a11fe3f99895dab7b22a994cec3ddefd
CarlosEsteban19 <carlos.esteban19@outlook.com>
2024-12-22 18:38:16 -0400

    df

--- supervised_learning/decision_tree/2-build_decision_tree.py
+++ supervised_learning/decision_tree/2-build_decision_tree.py
@@ -4,7 +4,7 @@ import numpy as np
 
 
 class Node:
-    """node class"""
+    """Node class"""
     def __init__(self, feature=None, threshold=None, left_child=None,
                  right_child=None, is_root=False, depth=0):
         self.feature = feature
@@ -17,19 +17,15 @@ class Node:
         self.depth = depth
 
     def max_depth_below(self):
-        """max depth method"""
+        """max depth below method"""
         if self.is_leaf:
             return self.depth
-
-        left_depth = self.left_child.max_depth_below()\
-            if self.left_child else self.depth
-        right_depth = self.right_child.max_depth_below()\
-            if self.right_child else self.depth
-
-        return max(left_depth, right_depth)
+        else:
+            return max(self.left_child.max_depth_below(),
+                       self.right_child.max_depth_below())
 
     def count_nodes_below(self, only_leaves=False):
-        """count nodes method"""
+        """count nodes below method"""
         if self.is_leaf:
             return 1
 
@@ -43,40 +39,20 @@ class Node:
         return 1 + left_count + right_count
 
     def __str__(self):
-        """str rep method"""
-        if self.is_root:
-            node_str = f"root [feature={self.feature}, threshold={self.threshold}]\n"
+        """String representation of the node and its children"""
+        if self.is_leaf:
+            return "-> leaf [value={}]".format(self.sub_population)
         else:
-            node_str = f"node [feature={self.feature}, threshold={self.threshold}]\n"
-
-        if self.left_child:
-            node_str += left_child_add_prefix(self.left_child.__str__())
-        if self.right_child:
-            node_str += right_child_add_prefix(self.right_child.__str__())
-        return node_str
-
-
-def left_child_add_prefix(text):
-    """left child prefix method"""
-    lines = text.split("\n")
-    new_text = "    +--"+lines[0]+"\n"
-    for x in lines[1:]:
-        new_text += ("    |  "+x)+"\n"
-    return (new_text)
-
-
-def right_child_add_prefix(text):
-    """right child prefix method"""
-    lines = text.split("\n")
-    new_text = "    +---> " + lines[0] + "\n"
-    for x in lines[1:]:
-        if x.strip():
-            new_text += "       " + x + "\n"
-    return new_text
+            left_str = self.left_child_add_prefix(
+                str(self.left_child)) if self.left_child else ""
+            right_str = self.right_child_add_prefix(
+                str(self.right_child)) if self.right_child else ""
+            return (f"[feature={self.feature}, threshold={self.threshold}]\n"
+                    f"{left_str}{right_str}").strip()
 
 
 class Leaf(Node):
-    """leaf class"""
+    """Leaf class"""
     def __init__(self, value, depth=None):
         super().__init__()
         self.value = value
@@ -84,20 +60,20 @@ class Leaf(Node):
         self.depth = depth
 
     def max_depth_below(self):
-        """max depth method"""
+        """max depth below method"""
         return self.depth
 
     def count_nodes_below(self, only_leaves=False):
-        """count nodes mthod"""
+        """count nodes below method"""
         return 1
 
     def __str__(self):
-        """str rep method"""
-        return f"leaf [value={self.value}]"
+        """String representation of a leaf node"""
+        return f"-> leaf [value={self.value}]"
 
 
 class Decision_Tree():
-    """decision tree class"""
+    """Decision tree class"""
     def __init__(self, max_depth=10, min_pop=1, seed=0,
                  split_criterion="random", root=None):
         self.rng = np.random.default_rng(seed)
@@ -121,5 +97,5 @@ class Decision_Tree():
         return self.root.count_nodes_below(only_leaves=only_leaves)
 
     def __str__(self):
-        """str rep method"""
+        """String representation of the decision tree"""
         return self.root.__str__()
