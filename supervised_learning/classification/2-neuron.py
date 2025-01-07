0d20477c83d2af5ca544b86e06b011013f26c5da
CarlosEsteban19 <carlos.esteban19@outlook.com>
2025-01-07 12:38:10 -0400

    task 2

---
+++ supervised_learning/classification/2-neuron.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""classification project"""
+import numpy as np
+
+
+class Neuron:
+    """Neuron class doc"""
+
+    def __init__(self, nx):
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+        self.nx = nx
+
+    @property
+    def W(self):
+        """weights vector for the neuron"""
+        return (self.__W)
+
+    @property
+    def b(self):
+        """bias for the neuron"""
+        return (self.__b)
+
+    @property
+    def A(self):
+        """activated output of the neuron"""
+        return (self.__A)
+
+    def forward_prop(self, X):
+        """Calculates the forward propagation of the neuron"""
+        z = np.matmul(self.W, X) + self.b
+        self.__A = 1 / (1 + (np.exp(-z)))
+        return (self.A)
