fc51a8fdb9a974bfa5118e9ba2dafaa3af692695
CarlosEsteban19 <carlos.esteban19@outlook.com>
2025-01-08 20:07:29 -0400

    task 25

---
+++ supervised_learning/classification/25-one_hot_decode.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""classification project"""
+import numpy as np
+
+
+def one_hot_decode(one_hot):
+    """converts a one-hot matrix into a numeric vector of labels"""
+    if type(one_hot) is not np.ndarray or len(one_hot.shape) != 2:
+        return None
+    vector = one_hot.transpose().argmax(axis=1)
+    return vector
