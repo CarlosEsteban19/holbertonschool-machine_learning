a3d5b1618a43f6b9b1cb858d050a8cb6c93d5c38
CarlosEsteban19 <carlos.esteban19@outlook.com>
2025-01-17 14:46:56 -0400

    .

---
+++ supervised_learning/tensorflow/0-create_placeholders.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Tensorflow project"""
+import tensorflow.compat.v1 as tf
+
+
+def create_placeholders(nx, classes):
+    """Create placeholders for the network"""
+    x = tf.placeholder(tf.float32, shape=[None, nx], name='x')
+    y = tf.placeholder(tf.float32, shape=[None, classes], name='y')
+    return x, y
