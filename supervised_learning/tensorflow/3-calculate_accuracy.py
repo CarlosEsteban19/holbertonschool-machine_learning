24b2f9dbf3d9c4d228b6e38c888d5a7d54b0be53
CarlosEsteban19 <carlos.esteban19@outlook.com>
2025-01-17 14:56:24 -0400

    .

---
+++ supervised_learning/tensorflow/3-calculate_accuracy.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""tensorflow project"""
+import tensorflow.compat.v1 as tf
+
+
+def calculate_accuracy(y, y_pred):
+    """documentation documentation documentation"""
+    correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_pred, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    return accuracy
