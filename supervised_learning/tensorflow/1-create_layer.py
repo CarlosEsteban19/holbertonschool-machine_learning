2e1e505233d0516e8c546c0be67941e336e3c0c1
CarlosEsteban19 <carlos.esteban19@outlook.com>
2025-01-17 14:50:47 -0400

    .

---
+++ supervised_learning/tensorflow/1-create_layer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""tensorflow project"""
+import tensorflow.compat.v1 as tf
+
+
+def create_layer(prev, n, activation):
+    """create a layer for tensorflow"""
+    init = tf.keras.initializers.VarianceScaling(mode='fan_avg')
+    layer = tf.layers.Dense(n,
+                            activation=activation,
+                            kernel_initializer=init,
+                            name='layer')
+    return layer(prev)
