c3590db27ff1007cd02b5690bb6d2e90a9d53219
CarlosEsteban19 <carlos.esteban19@outlook.com>
2025-01-19 14:35:35 -0400

    .

--- supervised_learning/keras/11-config.py
+++ supervised_learning/keras/11-config.py
@@ -1,23 +1,19 @@
 #!/usr/bin/env python3
 """keras project"""
 import tensorflow.keras as K
-import json
 
 
 def save_config(network, filename):
     """
     Saves a model configuration in JSON format"""
-    config = network.get_config()
-
-    with open(filename, 'w') as json_file:
-        json.dump(config, json_file)
+    config = network.to_json()
+    with open(filename, 'w') as f:
+        f.write(config)
 
 
 def load_config(filename):
     """Loads a model with a specific configuration"""
-    with open(filename, 'r') as json_file:
-        config = json.load(json_file)
-
-    model = K.models.Sequential.from_config(config)
-
+    with open(filename, 'r') as f:
+        config = f.read()
+    model = K.models.model_from_json(config)
     return model
