9740a899be2df710c42b9801ec738497db93822f
CarlosEsteban19 <carlos.esteban19@outlook.com>
2025-01-19 12:54:14 -0400

    .

---
+++ supervised_learning/keras/1-input.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+import tensorflow.keras as K
+
+
+def build_model(nx, layers, activations, lambtha, keep_prob):
+    """Builds a neural network using the functional API of Keras"""
+    inputs = K.Input(shape=(nx,))
+
+    regularizer = K.regularizers.L2(lambtha)
+
+    x = K.layers.Dense(layers[0], activation=activations[0],
+                       kernel_regularizer=regularizer)(inputs)
+    x = K.layers.Dropout(1 - keep_prob)(x)
+
+    for i in range(1, len(layers)):
+        x = K.layers.Dense(layers[i], activation=activations[i],
+                           kernel_regularizer=regularizer)(x)
+        if i < len(layers) - 1:
+            x = K.layers.Dropout(1 - keep_prob)(x)
+
+    model = K.Model(inputs=inputs, outputs=x)
+
+    return model
