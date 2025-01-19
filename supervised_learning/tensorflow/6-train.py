c03f7029137e6568fd6b819481d9e2c658517718
CarlosEsteban19 <carlos.esteban19@outlook.com>
2025-01-19 12:25:50 -0400

    .

---
+++ supervised_learning/tensorflow/6-train.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""tensorflow project"""
+import tensorflow.compat.v1 as tf
+
+
+def create_train_op(loss, alpha):
+    """Creates the training operation for the network using gradient descent"""
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate=alpha)
+    train_op = optimizer.minimize(loss)
+    return train_op
