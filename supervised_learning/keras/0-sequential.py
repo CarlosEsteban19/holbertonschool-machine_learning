1048b968bdb43f7dd5cbbfeb61cd463650bb4033
CarlosEsteban19 <carlos.esteban19@outlook.com>
2025-01-19 12:51:11 -0400

    .

--- supervised_learning/keras/0-sequential.py
+++ supervised_learning/keras/0-sequential.py
@@ -1,22 +1,22 @@
 #!/usr/bin/env python3
 """keras project"""
-import tensorflow.compat.v1 as tf
+import tensorflow.keras as K
 
 
-def evaluate(X, Y, save_path):
-    """Evaluates the output of a neural network."""
-    with tf.Session() as sess:
-        saver = tf.train.import_meta_graph(save_path + '.meta')
-        saver.restore(sess, save_path)
+def build_model(nx, layers, activations, lambtha, keep_prob):
+    """Builds a neural network with the Keras library"""
+    model = K.Sequential()
+    regularizer = K.regularizers.L2(lambtha)
 
-        x = tf.get_collection('x')[0]
-        y = tf.get_collection('y')[0]
-        y_pred = tf.get_collection('y_pred')[0]
-        accuracy = tf.get_collection('accuracy')[0]
-        loss = tf.get_collection('loss')[0]
+    model.add(K.layers.Dense(
+        layers[0], activation=activations[0],
+        kernel_regularizer=regularizer, input_shape=(nx,)))
+    model.add(K.layers.Dropout(1 - keep_prob))
 
-        y_pred_val = sess.run(y_pred, feed_dict={x: X, y: Y})
-        accuracy_val = sess.run(accuracy, feed_dict={x: X, y: Y})
-        loss_val = sess.run(loss, feed_dict={x: X, y: Y})
+    for i in range(1, len(layers)):
+        model.add(K.layers.Dense(layers[i], activation=activations[i],
+                                 kernel_regularizer=regularizer))
+        if i < len(layers) - 1:  # No dropout on the output layer
+            model.add(K.layers.Dropout(1 - keep_prob))
 
-    return y_pred_val, accuracy_val, loss_val
+    return model
