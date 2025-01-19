614e771aaa87298e84897de3fe70170427e6a2c4
CarlosEsteban19 <carlos.esteban19@outlook.com>
2025-01-19 13:10:11 -0400

    .

---
+++ supervised_learning/keras/4-train.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""keras project"""
+
+
+def train_model(network, data, labels, batch_size,
+                epochs, verbose=True, shuffle=False):
+    """Trains a model using mini-batch gradient descent"""
+    history = network.fit(
+        x=data,
+        y=labels,
+        batch_size=batch_size,
+        epochs=epochs,
+        verbose=verbose,
+        shuffle=shuffle
+    )
+    return history
