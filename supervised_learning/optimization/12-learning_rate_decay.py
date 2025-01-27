8d6c1635ce33dda091bb8ea845014a844703c93e
CarlosEsteban19 <carlos.esteban19@outlook.com>
2025-01-26 22:43:48 -0400

    with inverse time decay

--- supervised_learning/optimization/12-learning_rate_decay.py
+++ supervised_learning/optimization/12-learning_rate_decay.py
@@ -7,7 +7,7 @@ def learning_rate_decay(alpha, decay_rate, decay_step):
     """
     Creates a learning rate decay operation using inverse time decay.
     """
-    return tf.keras.optimizers.schedules.ExponentialDecay(
+    return tf.keras.optimizers.schedules.InverseTimeDecay(
         initial_learning_rate=alpha,
         decay_steps=decay_step,
         decay_rate=decay_rate,
