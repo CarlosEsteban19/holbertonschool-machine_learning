1499ff2e841f03cf1e65f96d3f5e50a9e71649bf
CarlosEsteban19 <carlos.esteban19@outlook.com>
2025-01-28 17:01:30 -0400

    precise

---
+++ supervised_learning/error_analysis/2-precision.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""holberton error analysis"""
+import numpy as np
+
+
+def precision(confusion):
+    """
+    Calculates the precision for each class in a confusion matrix.
+    """
+    true_positives = np.diag(confusion)
+
+    false_positives = np.sum(confusion, axis=0) - true_positives
+
+    precision = true_positives / (true_positives + false_positives)
+
+    return precision
