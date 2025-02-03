2b0aa1ac8256128dd88295f4e7336dd4a4722803
CarlosEsteban19 <carlos.esteban19@outlook.com>
2025-02-02 21:16:14 -0400

    con tensor flow

--- supervised_learning/regularization/6-dropout_create_layer.py
+++ supervised_learning/regularization/6-dropout_create_layer.py
@@ -1,22 +1,22 @@
 #!/usr/bin/env python3
 """holberton regularization"""
-import numpy as np
+import tensorflow as tf
 
 
 def dropout_create_layer(prev, n, activation, keep_prob, training=True):
     """dropout create ayer"""
-    m = prev.shape[1]
-    W = np.random.randn(n, prev.shape[0]) * np.sqrt(2. / prev.shape[0])
-    b = np.zeros((n, 1))
-    Z = np.dot(W, prev) + b
+    W = tf.random.normal(
+        [n, prev.shape[0]], stddev=tf.sqrt(2. / prev.shape[0]))
+    b = tf.zeros([n, 1])
+    Z = tf.add(tf.matmul(W, prev), b)
 
     A = activation(Z)
 
     if training:
-        D = np.random.rand(*A.shape) < keep_prob
+        D = tf.cast(tf.random.uniform(A.shape) < keep_prob, tf.float32)
         A = A * D
         A /= keep_prob
     else:
-        D = np.ones_like(A)
+        D = tf.ones_like(A)
 
     return A
