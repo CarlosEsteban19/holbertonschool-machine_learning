0089196e1e1e899771815ac3aead378ec2cd39f1
CarlosEsteban19 <carlos.esteban19@outlook.com>
2025-02-02 20:46:55 -0400

    convert to tensor

--- supervised_learning/regularization/2-l2_reg_cost.py
+++ supervised_learning/regularization/2-l2_reg_cost.py
@@ -5,5 +5,5 @@ import tensorflow as tf
 
 def l2_reg_cost(cost, model):
     """Calculates the cost of a neural network with L2 regularization."""
-    l2_losses = tf.add_n(model.losses)
+    l2_losses = tf.convert_to_tensor([loss.numpy() for loss in model.losses])
     return cost + l2_losses
