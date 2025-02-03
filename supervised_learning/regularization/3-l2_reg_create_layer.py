edc4e9e14ea05566042d8c320bbc9365f16e989d
CarlosEsteban19 <carlos.esteban19@outlook.com>
2025-02-02 20:54:52 -0400

    fix

--- supervised_learning/regularization/3-l2_reg_create_layer.py
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -4,15 +4,13 @@ import tensorflow as tf
 
 
 def l2_reg_create_layer(prev, n, activation, lambtha):
-    """creates a neural network layer in tensorFlow"""
-    tf.random.set_seed(42)
+    """
+    Creates a layer with L2 regularization
+    """
+    kernel_initializer = tf.keras.initializers.GlorotUniform()
 
-    regularizer = tf.keras.regularizers.L2(lambtha)
-    initializer = tf.keras.initializers.VarianceScaling(mode='fan_avg')
-    layer = tf.keras.layers.Dense(
-        units=n,
-        activation=activation,
-        kernel_initializer=initializer,
-        kernel_regularizer=regularizer
-    )
-    return layer(prev)
+    dense_layer = tf.keras.layers.Dense(
+        n, activation=activation, kernel_initializer=kernel_initializer,
+        kernel_regularizer=tf.keras.regularizers.l2(lambtha))
+
+    return dense_layer(prev)
