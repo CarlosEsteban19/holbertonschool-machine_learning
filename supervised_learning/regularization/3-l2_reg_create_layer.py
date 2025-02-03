e6b007feed77da1b6c4f5a4209577ced9e1affe9
CarlosEsteban19 <carlos.esteban19@outlook.com>
2025-02-02 20:57:45 -0400

    indent?

--- supervised_learning/regularization/3-l2_reg_create_layer.py
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -5,12 +5,13 @@ import tensorflow as tf
 
 def l2_reg_create_layer(prev, n, activation, lambtha):
     """
-    Creates a layer with L2 regularization
+    Creates a layer with L2 regularization.
     """
     kernel_initializer = tf.keras.initializers.GlorotUniform()
 
-    dense_layer = tf.keras.layers.Dense(
-        n, activation=activation, kernel_initializer=kernel_initializer,
-        kernel_regularizer=tf.keras.regularizers.l2(lambtha))
+    dense_layer = tf.keras.layers.Dense(n,
+                                        activation=activation,
+                                        kernel_initializer=kernel_initializer,
+                                        kernel_regularizer=tf.keras.regularizers.l2(lambtha))
 
     return dense_layer(prev)
