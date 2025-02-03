45db806515d6722df475ea31e61b60a8c7ca74ec
CarlosEsteban19 <carlos.esteban19@outlook.com>
2025-02-02 20:42:18 -0400

    creates layer

---
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""holberton regularization"""
+import tensorflow as tf
+
+
+def l2_reg_create_layer(prev, n, activation, lambtha):
+    """creates a neural network layer in tensorFlow"""
+    regularizer = tf.keras.regularizers.L2(lambtha)
+    initializer = tf.keras.initializers.VarianceScaling(mode='fan_avg')
+    layer = tf.keras.layers.Dense(
+        units=n,
+        activation=activation,
+        kernel_initializer=initializer,
+        kernel_regularizer=regularizer
+    )
+    return layer(prev)
