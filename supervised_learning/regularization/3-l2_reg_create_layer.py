6f45566a24cdd96000483f57741641fa259838fd
CarlosEsteban19 <carlos.esteban19@outlook.com>
2025-02-02 20:51:03 -0400

    set random seed

--- supervised_learning/regularization/3-l2_reg_create_layer.py
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -5,6 +5,8 @@ import tensorflow as tf
 
 def l2_reg_create_layer(prev, n, activation, lambtha):
     """creates a neural network layer in tensorFlow"""
+    tf.random.set_seed(42)
+
     regularizer = tf.keras.regularizers.L2(lambtha)
     initializer = tf.keras.initializers.VarianceScaling(mode='fan_avg')
     layer = tf.keras.layers.Dense(
